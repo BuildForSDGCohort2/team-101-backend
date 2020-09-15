c92ef05d6d245aa81cf8479544b2c9f9e7204445
Adebayo Osinulu <33939983+captbilard@users.noreply.github.com>
2020-09-15 23:03:18 +0100

    Update serializers.py
    Updated as recommended, Kindly review and give feedback

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,11 +1,7 @@
 from rest_framework import serializers
-
-
 from dj_rest_auth.registration.serializers import RegisterSerializer
 
 from .models import User
-
-
 
 class CustomRegisterSerializer(RegisterSerializer, serializers.ModelSerializer):
     """Custom serializer to handle registration"""
@@ -13,7 +9,11 @@ class CustomRegisterSerializer(RegisterSerializer, serializers.ModelSerializer):
     password2 = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'town_city', 'state', 'country']
+        fields = [
+            'username', 'email', 'password1',
+            'password2', 'first_name', 'last_name',
+            'town_city', 'state', 'country'
+        ]
     def save(self, request):
         user = User(
             username = self.validated_data['username'],
@@ -30,9 +30,11 @@ class CustomRegisterSerializer(RegisterSerializer, serializers.ModelSerializer):
         user.save()
         return user
 
-
 class CustomUserDetailsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'town_city', 'state', 'country', 'is_maintainer']
-
+        fields = [
+            'username', 'email', 'first_name',
+            'last_name', 'town_city', 'state',
+            'country', 'is_maintainer'
+        ]
