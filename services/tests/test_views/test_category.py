ee9eb7fd2c32774c935b65f9239e9fff1ac4c188
damephena <okoyeifenna24@gmail.com>
2020-09-28 03:55:29 +0100

    changes for duplicacy

--- services/tests/test_views/test_category.py
+++ services/tests/test_views/test_category.py
@@ -40,9 +40,9 @@ class CategoryTestCase(APITestCase):
         self.user = get_user_model().objects.get(email='contry@example.com')
         self.contrib2 = get_user_model().objects.get(email='contry@example.com')
 
-    def test_maintainer_can_create_category(self):
+    def maintainer_login(self):
 
-        '''Test maintainer or admin can create category'''
+        '''Method for maintainer login'''
         maintainer = get_user_model().objects.get(email='test@example.com')
         maintainer.is_maintainer = True
         maintainer.is_contributor = False
@@ -55,12 +55,9 @@ class CategoryTestCase(APITestCase):
         token = login.data['access_token']
         self.client.credentials(HTTP_AUTHORIZATION='Bearer '+ token)
 
-        response = self.client.post('/api/services/category/', data={'name': 'Technology'})
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    def contributor_login(self):
 
-    def test_contributor_fails_create_category(self):
-
-        '''Test contributor cannot create a category'''
+        '''Method for contributor login'''
         login = self.client.post(reverse('rest_login'),
             data={
                 'email': 'contry@example.com',
@@ -69,19 +66,40 @@ class CategoryTestCase(APITestCase):
         token = login.data['access_token']
         self.client.credentials(HTTP_AUTHORIZATION='Bearer '+ token)
 
+    def make_maintainer(self):
+
+        '''Method to switch to `contributor` type'''
+        pseudo_maintainer = get_user_model().objects.get(email='contry@example.com')
+        pseudo_maintainer.is_maintainer = True
+        pseudo_maintainer.is_contributor = False
+        pseudo_maintainer.save()
+
+    def make_contributor(self):
+
+        '''Method to switch to `maintainer` type'''
+        pseudo_maintainer = get_user_model().objects.get(email='contry@example.com')
+        pseudo_maintainer.is_maintainer = False
+        pseudo_maintainer.is_contributor = True
+        pseudo_maintainer.save()
+
+    def test_maintainer_can_create_category(self):
+
+        '''Test maintainer or admin can create category'''
+        self.maintainer_login()
+        response = self.client.post('/api/services/category/', data={'name': 'Technology'})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_contributor_fails_create_category(self):
+
+        '''Test contributor cannot create a category'''
+        self.contributor_login()
         response = self.client.post('/api/services/category/', data={'name': 'Technology'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_contributor_can_get_category(self):
 
         '''Test contributor can get a category'''
-        login = self.client.post(reverse('rest_login'), data={
-            'email': 'contry@example.com',
-            'password': 'passworddd1234',
-        })
-        token = login.data['access_token']
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer '+ token)
-
+        self.contributor_login()
         response = self.client.get('/api/services/category/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -92,7 +110,7 @@ class CategoryTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_anonymous_user_fails_get_category(self):
-    
+
         '''Test anonymous user cannot create a category'''
         response = self.client.post('/api/services/category/', data={'name': 'Agriculture'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
@@ -100,18 +118,7 @@ class CategoryTestCase(APITestCase):
     def test_maintainer_can_delete_category(self):
 
         '''Test maintainer or admin can delete category'''
-        maintainer = get_user_model().objects.get(email='test@example.com')
-        maintainer.is_maintainer = True
-        maintainer.is_contributor = False
-        maintainer.save()
-
-        login = self.client.post(reverse('rest_login'), data={
-            'email': 'test@example.com',
-            'password': 'passworddd1234',
-        })
-        token = login.data['access_token']
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer '+ token)
-
+        self.maintainer_login()
         category = self.client.post('/api/services/category/', data={'name': 'Technology'})
         response = self.client.delete('/api/services/category/' + str(category.data['id']) + '/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
@@ -119,42 +126,19 @@ class CategoryTestCase(APITestCase):
     def test_contributor_fails_delete_category(self):
 
         '''Test contributor cannot delete a category'''
-        login = self.client.post(reverse('rest_login'), data={
-            'email': 'contry@example.com',
-            'password': 'passworddd1234',
-        })
-        token = login.data['access_token']
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer '+ token)
-
-        pseudo_maintainer = get_user_model().objects.get(email='contry@example.com')
-        pseudo_maintainer.is_maintainer = True
-        pseudo_maintainer.is_contributor = False
-        pseudo_maintainer.save()
+        self.contributor_login()
+        self.make_maintainer()
 
         category = self.client.post('/api/services/category/', data={'name': 'Technology'})
-
-        pseudo_maintainer = get_user_model().objects.get(email='contry@example.com')
-        pseudo_maintainer.is_maintainer = False
-        pseudo_maintainer.is_contributor = True
-        pseudo_maintainer.save()
-
+        self.make_contributor()
         response = self.client.delete('/api/services/category/' + str(category.data['id']) + '/')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_anonymous_user_fails_delete_category(self):
 
         '''Test anonymous user cannot delete a category'''
-        login = self.client.post(reverse('rest_login'), data={
-            'email': 'contry@example.com',
-            'password': 'passworddd1234',
-        })
-        token = login.data['access_token']
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer '+ token)
-
-        pseudo_maintainer = get_user_model().objects.get(email='contry@example.com')
-        pseudo_maintainer.is_maintainer = True
-        pseudo_maintainer.is_contributor = False
-        pseudo_maintainer.save()
+        self.contributor_login()
+        self.make_maintainer()
 
         category = self.client.post('/api/services/category/', data={'name': 'Technology'})
 
@@ -163,20 +147,9 @@ class CategoryTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_maintainer_can_update_category(self):
-    
+
         '''Test maintainer or admin can edit/update category'''
-        maintainer = get_user_model().objects.get(email='test@example.com')
-        maintainer.is_maintainer = True
-        maintainer.is_contributor = False
-        maintainer.save()
-
-        login = self.client.post(reverse('rest_login'), data={
-            'email': 'test@example.com',
-            'password': 'passworddd1234',
-        })
-        token = login.data['access_token']
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer '+ token)
-
+        self.maintainer_login()
         category = self.client.post('/api/services/category/', data={'name': 'Technology'})
         url = '/api/services/category/' + str(category.data['id']) + '/'
 
@@ -186,24 +159,11 @@ class CategoryTestCase(APITestCase):
     def test_contributor_fails_update_category(self):
 
         '''Test contributor cannot update/edit a category'''
-        login = self.client.post(reverse('rest_login'), data={
-            'email': 'contry@example.com',
-            'password': 'passworddd1234',
-        })
-        token = login.data['access_token']
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer '+ token)
-
-        pseudo_maintainer = get_user_model().objects.get(email='contry@example.com')
-        pseudo_maintainer.is_maintainer = True
-        pseudo_maintainer.is_contributor = False
-        pseudo_maintainer.save()
+        self.contributor_login()
+        self.make_maintainer()
 
         category = self.client.post('/api/services/category/', data={'name': 'Technology'})
-
-        pseudo_maintainer = get_user_model().objects.get(email='contry@example.com')
-        pseudo_maintainer.is_maintainer = False
-        pseudo_maintainer.is_contributor = True
-        pseudo_maintainer.save()
+        self.make_contributor()
 
         url = '/api/services/category/' + str(category.data['id']) + '/'
         response = self.client.put(url, data={'name': 'not gonna work'})
@@ -212,17 +172,8 @@ class CategoryTestCase(APITestCase):
     def test_anonymous_user_fails_update_category(self):
 
         '''Test anonymous user cannot update/edit a category'''
-        login = self.client.post(reverse('rest_login'), data={
-            'email': 'contry@example.com',
-            'password': 'passworddd1234',
-        })
-        token = login.data['access_token']
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer '+ token)
-
-        pseudo_maintainer = get_user_model().objects.get(email='contry@example.com')
-        pseudo_maintainer.is_maintainer = True
-        pseudo_maintainer.is_contributor = False
-        pseudo_maintainer.save()
+        self.contributor_login()
+        self.make_maintainer()
 
         category = self.client.post('/api/services/category/', data={'name': 'Technology'})
         url = '/api/services/category/' + str(category.data['id']) + '/'
