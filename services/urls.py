33683fc7cf9cd3be1bd62864bd67297b9d18de6a
damephena <okoyeifenna24@gmail.com>
2020-09-30 18:27:31 +0100

    CRUD for tag endpoint

--- services/urls.py
+++ services/urls.py
@@ -4,7 +4,7 @@ import services.views as views
 
 router = DefaultRouter()
 router.register('category', views.CategoryViewset, basename='category')
+router.register('tag', views.TagViewset, basename='tag')
+router.register('item', views.ItemViewset, basename='item')
 
-urlpatterns = [
-
-] + router.urls
+urlpatterns = router.urls
